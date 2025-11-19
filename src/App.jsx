b7e3513957e16c66dc1fe4468fdf7d9fd51fe22e
dpa313 <dpa.github@gmail.com>
2025-11-19 21:15:00 +0600

    toggle-background-added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Counter from './components/Counter'
 import Todo from './components/Todo'
 import Meals from './components/Meals'
+import ToggleBackgroundColor from './components/ToggleBackgroundColor'
 
 const App = () => {
   return (
     <>
       {/* <Counter /> */}
       {/* <Todo /> */}
-      <Meals />
+      {/* <Meals /> */}
+      <ToggleBackgroundColor />
     </>
   )
 }
---
+++ src/components/ToggleBackgroundColor.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import '../style.css'
+
+const ToggleBackgroundColor = () => {
+    const [backgroundColor,setBackgroundColor] = useState("white")
+    const [textColor,setTextColor] = useState("#1b1b1b")
+    const [buttonStyle,setButtonStyle] = useState("white")
+
+    const handleToggle = () =>{
+        setBackgroundColor(backgroundColor === "white" ? "#1b1b1b" : "white")
+        setTextColor(textColor === "#1b1b1b" ? "#ffa31a": "#1b1b1b")
+        setButtonStyle(buttonStyle === "white"? "#1b1b1b" : "white")
+    }
+  return (
+    <div style={{ backgroundColor,color:textColor}}>
+        <button style={{buttonStyle,color:textColor, border: `2px solid ${textColor}`}} onClick={handleToggle}>{backgroundColor === "white" ? "Black Theme" : "White Theme"}</button>
+        <section className='content'>
+            <h1>WELCOME TO A <br /> NEW WORLD...</h1>
+        </section>
+    </div>
+  )
+}
+
+export default ToggleBackgroundColor
